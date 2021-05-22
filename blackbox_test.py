54a2ee0b1d52e61f601f5d69cdf8010fa6b7d809
michaelgadda <gaddami@oregonstate.edu>
2021-05-22 12:02:17 -0700

    initial blackbox test

---
+++ blackbox_test.py
@@ -0,0 +1,25 @@
+import unittest 
+from leap_year import leap_year as lp
+
+
+
+class TestPalindrome(unittest.TestCase):
+    
+    def test_corrrectness(self):
+    	self.assertEqual(lp(2000), True)
+    	self.assertEqual(lp(1999), False)
+    	self.assertEqual(lp(0), False)
+   
+    def test_type(self):
+    	self.assertEqual(lp("!"), False)
+    	self.assertEqual(lp(" ```"), False)
+    	self.assertNotEqual(lp(2000), False)
+    	self.assertNotEqual(lp(4), False)
+
+    def test_null(self): 
+    	self.assertEqual(lp(" "), False)
+    	self.assertEqual(lp("  "), False)
+    	self.assertNotEqual(lp("  "), False)
+
+if __name__ == '__main__': 
+	unittest.main()
